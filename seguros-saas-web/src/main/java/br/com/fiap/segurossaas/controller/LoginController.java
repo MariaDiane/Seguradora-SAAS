e86c346a7c781bb0124a53e9f3a3f718b2891c11
Helena Strada <h.strada@hotmail.com>
2017-09-17 14:39:40 -0300

    Ajustado a listagem da parte web.

--- seguros-saas-web/src/main/java/br/com/fiap/segurossaas/controller/LoginController.java
+++ seguros-saas-web/src/main/java/br/com/fiap/segurossaas/controller/LoginController.java
@@ -15,27 +15,27 @@ import br.com.fiap.segurossaas.vo.LoginVO;
 
 @Controller
 public class LoginController {
-	
+
 	@Autowired
 	private LojaService lojaService;
-	
+
 	@Autowired
 	private ContratoService contratoService;
-	
+
 	@RequestMapping("/")
-	public String index(){
+	public String index() {
 		return "login";
 	}
-	
+
 	@PostMapping("/logar")
-	public String logar(@RequestParam String usuario, @RequestParam String senha, Model model ){
+	public String logar(@RequestParam String usuario, @RequestParam String senha, Model model) {
 		Loja loja = lojaService.buscaLoja(new LoginVO(usuario, senha));
-		if(loja != null){
+		if (loja != null) {
 			Iterable<Contrato> contratos = contratoService.obterTodos(loja);
 			model.addAttribute("contratos", contratos);
+			model.addAttribute("loja", loja);
 			return "home";
-		}
-		else
+		} else
 			return "login";
 	}
 }
